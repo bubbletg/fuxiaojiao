90255fa44ea88bf900d2788725d2b6c0dbe0bd7a
BubbleTg <tg_z@qq.com>
2021-11-12 16:42:31 +0800

    ✨ feat: 第一次登录默认注册，且普通用户权限

--- uniCloud-aliyun/cloudfunctions/user/service/user.js
+++ uniCloud-aliyun/cloudfunctions/user/service/user.js
@@ -2,18 +2,29 @@ const { Service } = require("uni-cloud-router")
 const uniID = require("uni-id")
 module.exports = class UserService extends Service {
   async userLogin({ code, userInfo }, uid) {
-    // 这里会根据code 创建用户信息，并返回
-    const loginInfo = await uniID.loginByWeixin({ code })
     // 拿到用户信息
-    const { nickName, avatarUrl, openId } = userInfo
+    let { nickName, avatarUrl, openId, role } = userInfo
+
+    let loginInfo = null
+    // 不存在uid 表示用户为第一次注册
+    if (!uid) {
+      // 这里会根据code 创建用户信息
+      loginInfo = await uniID.loginByWeixin({ code })
+      uid = loginInfo.uid
+      // 权限为null/undefined，只有新创建用户才会这样
+      // 初始化为普通用户
+      role = ["normal_user"]
+    }
+
     // 把用户信息更新
     await uniID.updateUser({
       uid,
       nickName,
       avatarUrl,
-      openId
+      openId,
+      role
     })
     // 查询更新后的
-    return await uniID.getUserInfo({ uid: loginInfo.uid })
+    return await uniID.getUserInfo({ uid })
   }
 }
